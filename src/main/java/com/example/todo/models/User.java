fbe83c27860871cacf63b55f27e8588075d4f245
AG1713 <avdhootgole@gmail.com>
2025-07-20 09:29:09 +0530

    Java version changed to 17

--- src/main/java/com/example/todo/models/User.java
+++ src/main/java/com/example/todo/models/User.java
@@ -10,6 +10,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String username;
     private String password;
 
